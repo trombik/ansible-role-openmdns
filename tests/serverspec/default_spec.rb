fb1a8f725cbb4d8689e9e01ea0f7b0b3b4615b5c
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-12-07 15:36:02 +0900

    support FreeBSD

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -6,15 +6,37 @@ service = "mdnsd"
 _user    = "_mdnsd"
 _group   = "_mdnsd"
 ports = [5353]
+default_owner = "root"
+default_group = "root"
+
+case os[:family]
+when "freebsd"
+  default_group = "wheel"
+when "openbsd"
+  default_group = "wheel"
+end
 
 describe package(package) do
   it { should be_installed }
 end
 
-describe command("rcctl get #{service} flags") do
-  its(:exit_status) { should eq 0 }
-  its(:stderr) { should eq "" }
-  its(:stdout) { should eq "em0\n" }
+
+case os[:family]
+when "openbsd"
+  describe command("rcctl get #{service} flags") do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq "" }
+    its(:stdout) { should eq "em0\n" }
+  end
+when "freebsd"
+  describe file("/etc/rc.conf.d/#{service}") do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by default_owner }
+    it { should be_grouped_into default_group }
+    it { should be_mode 644 }
+    its(:content) { should match(/^#{service}_flags="em0"$/) }
+  end
 end
 
 describe service(service) do
